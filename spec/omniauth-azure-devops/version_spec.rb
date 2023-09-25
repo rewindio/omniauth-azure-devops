b8ad38b4988282dc5c0f16bfe797a4e1a63a6521
Mandeep Khinda <32844597+mkhinda29@users.noreply.github.com>
2023-09-25 11:14:57 -0400

    rubocop autocorrect, changelog, version

--- spec/omniauth-azure-devops/version_spec.rb
+++ spec/omniauth-azure-devops/version_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe Omniauth::AzureDevops do
-  it 'should have a version number' do
+  it 'has a version number' do
     expect(Omniauth::AzureDevops::VERSION).not_to be_nil
   end
 end
--- spec/omniauth/strategies/azure_devops_spec.rb
+++ spec/omniauth/strategies/azure_devops_spec.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe OmniAuth::Strategies::AzureDevops do
-  before :each do
-    @request = double('Request')
-    allow(@request).to receive(:params) { {} }
-    allow(@request).to receive(:scheme) { 'https' }
-    allow(@request).to receive(:url) { '/auth/azure_devops/callback' }
-    allow(@request).to receive(:env) { { 'rack.input' => StringIO.new } }
-  end
-
   subject do
-    OmniAuth::Strategies::AzureDevops.new(nil, @options || {}).tap do |strategy|
+    described_class.new(nil, @options || {}).tap do |strategy|
       allow(strategy).to receive(:request) { @request }
     end
+  end
+
+  before do
+    @request = double('Request')
+    allow(@request).to receive_messages(params: {}, scheme: 'https', url: '/auth/azure_devops/callback')
+    allow(@request).to receive(:env) { { 'rack.input' => StringIO.new } }
   end
 
   describe '#client' do
@@ -36,11 +36,11 @@ describe OmniAuth::Strategies::AzureDevops do
       allow(subject).to receive(:raw_info).and_return(raw_info)
     end
 
-    it 'should return the correct name' do
+    it 'returns the correct name' do
       expect(subject.info[:name]).to eq('John Doe')
     end
 
-    it 'should return the correct email address' do
+    it 'returns the correct email address' do
       expect(subject.info[:email_address]).to eq('john.doe@example.com')
     end
   end
@@ -52,54 +52,57 @@ describe OmniAuth::Strategies::AzureDevops do
       allow(subject).to receive(:raw_info).and_return(raw_info)
     end
 
-    it 'should include the raw_info in extra data' do
+    it 'includes the raw_info in extra data' do
       expect(subject.extra[:raw_info]).to eq(raw_info)
     end
   end
 
   describe '#callback_path' do
-    it 'should have the correct callback path' do
+    it 'has the correct callback path' do
       expect(subject.callback_path).to eq('/auth/azure_devops/callback')
     end
   end
 
   describe '#callback_url' do
-    it 'should have the correct callback path' do
+    it 'has the correct callback path' do
       expect(subject.callback_url).to eq('/auth/azure_devops/callback')
     end
   end
 
   describe '#token_params' do
-    it 'should have the correct Content-Type' do
+    it 'has the correct Content-Type' do
       expect(subject.token_params[:headers]['Content-Type']).to eq('application/x-www-form-urlencoded')
     end
 
-    it 'should have the correct client_assertion_type' do
+    it 'has the correct client_assertion_type' do
       expect(subject.token_params[:client_assertion_type]).to eq('urn:ietf:params:oauth:client-assertion-type:jwt-bearer')
     end
 
-    it 'should have the correct grant_type' do
+    it 'has the correct grant_type' do
       expect(subject.token_params[:grant_type]).to eq('urn:ietf:params:oauth:grant-type:jwt-bearer')
     end
 
-    it 'should have the correct assertion' do
-      allow(@request).to receive(:params) { { 'code' => 'q0398109283019283019830192830192830192830129' } }
+    it 'has the correct assertion' do
+      allow(@request).to receive(:params).and_return({ 'code' => 'q0398109283019283019830192830192830192830129' })
       expect(subject.token_params[:assertion]).to eq('q0398109283019283019830192830192830192830129')
     end
 
-    it 'should have the correct redirect_uri' do
+    it 'has the correct redirect_uri' do
       expect(subject.token_params[:redirect_uri]).to eq('/auth/azure_devops/callback')
     end
   end
 
   describe '#raw_info' do
-    let(:access_token) { instance_double('AccessToken', get: instance_double('Response', parsed: { 'id' => '123', 'display_name' => 'John Doe', 'email_address' => 'john.doe@example.com' })) }
+    let(:access_token) do
+      instance_double(AccessToken,
+                      get: instance_double(Response, parsed: { 'id' => '123', 'display_name' => 'John Doe', 'email_address' => 'john.doe@example.com' }))
+    end
 
     before do
       allow(subject).to receive(:access_token).and_return(access_token)
     end
 
-    it 'should fetch and parse user profile information' do
+    it 'fetches and parse user profile information' do
       expect(subject.raw_info['id']).to eq('123')
       expect(subject.raw_info['display_name']).to eq('John Doe')
       expect(subject.raw_info['email_address']).to eq('john.doe@example.com')
